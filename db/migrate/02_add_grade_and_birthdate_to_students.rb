89ae437ad8c9734e7538552d53deeaeaceba9301
Edsel Caprice <caprice@mail.usf.edu>
2018-08-20 00:26:53 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,10 @@
 class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[5.1]
 
-
+def change
+  add_column :students, :grade, :integer
+  add_column :students, :birthdate, :string
+  end
+end
+    
 
 end
